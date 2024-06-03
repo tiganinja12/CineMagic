b992d9f72e5b37f98b759ec36636ebafa7584ab7
Daniel "zePiCzz" Mariano <danielmariano123.dm@gmail.com>
2024-06-03 15:03:41 +0100

    Login

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string|min:3',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials, $request->filled('remember'))) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('/');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,38 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+<<<<<<< Updated upstream
+=======
+
+// routes/web.php
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\PurchaseController;
+use App\Http\Controllers\ScreeningController;
+use App\Http\Controllers\HomeController;
+
+use App\Http\Controllers\Auth\LoginController;
+
+
+// Auth Routes
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+
+
+// Home route
+Route::get('/', [HomeController::class, 'index']);
+
+// Movie routes
+Route::get('/movies', [MovieController::class, 'index']);
+Route::get('/movies/{id}', [MovieController::class, 'show']);
+Route::resource('movies', MovieController::class);
+
+
+// Purchase routes
+Route::post('/purchases', [PurchaseController::class, 'store'])->middleware('auth:sanctum');
+
+// Screening routes
+Route::get('/screenings', [ScreeningController::class, 'index']);
+Route::get('/screenings/{id}', [ScreeningController::class, 'show']);
+>>>>>>> Stashed changes
