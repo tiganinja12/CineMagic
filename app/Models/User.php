4a03116f732ffb4fa8d1c473246ab34852b6fd02
Daniel "zePiCzz" Mariano <danielmariano123.dm@gmail.com>
2024-06-12 12:52:57 +0100

    ProfilePic and Starting PasswordReset

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -48,6 +48,6 @@ class User extends Authenticatable
 
     public function getProfilePictureUrlAttribute()
     {
-        return $this->photo_filename ? asset('storage/profile_pictures/' . $this->photo_filename) : asset('storage/profile_pictures/default.png');
+        return $this->photo_filename ? asset('storage/photos' . $this->photo_filename) : asset('storage/photos/default.png');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,42 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-// routes/web.php
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MovieController;
 use App\Http\Controllers\ScreeningController;
 use App\Http\Controllers\PurchaseController;
 use App\Http\Controllers\HomeController;
-
-
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\ProfileController;
 
-
+// Default route
+Route::get('/', function () {
+    return view('welcome');
+});
 
 // Auth routes
-    //Login
+Auth::routes(['verify' => true]);
+
+// Login routes
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login']);
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
-    //Register
+// Register routes
 Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('register', [RegisterController::class, 'register']);
 
-    //Profile
-Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'show'])->name('profile');
+// Password Reset routes
+Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
 
+// Profile route
+Route::get('/profile', [ProfileController::class, 'show'])->name('profile');
 
 // Home route
 Route::get('/', [HomeController::class, 'index']);
@@ -40,9 +45,7 @@ Route::get('/', [HomeController::class, 'index']);
 Route::resource('movies', MovieController::class);
 
 // Purchase routes
-Route::post('/purchases', [PurchaseController::class, 'store'])->middleware('auth:sanctum');
+Route::post('/purchases', [PurchaseController::class, 'store'])->middleware('auth');
 
 // Screening routes
-Route::get('/screenings', [ScreeningController::class, 'index']);
-Route::get('/screenings/{id}', [ScreeningController::class, 'show']);
 Route::resource('screenings', ScreeningController::class);
